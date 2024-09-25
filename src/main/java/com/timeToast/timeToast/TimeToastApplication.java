904309c7d7738b1e863035bae5aaa217c54bc847
yeiin <yeini0417@gmail.com>
2024-09-25 22:15:51 +0900

    init

---
+++ src/main/java/com/timeToast/timeToast/TimeToastApplication.java
@@ -0,0 +1,13 @@
+package com.timeToast.timeToast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeToastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeToastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timeToast/timeToast/TimeToastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeToast.timeToast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeToastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
