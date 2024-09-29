74fbbc0cac9e0fb6a9cd4fbd108fcb1ec40333a9
yeiin <yeini0417@gmail.com>
2024-09-29 21:02:16 +0900

    feat: login interceptor

---
+++ src/main/java/com/timeToast/timeToast/global/interceptor/LoginInterceptor.java
@@ -0,0 +1,8 @@
+package com.timeToast.timeToast.global.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+
+}
