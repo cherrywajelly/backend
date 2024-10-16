baca9f1807cfdfd34235e5e7305130c5c5863e81
yeiin <yeini0417@gmail.com>
2024-10-16 15:00:11 +0900

    refactor: 로그인 log 추가

--- src/main/java/com/timeToast/timeToast/service/jwt/JwtServiceImpl.java
+++ src/main/java/com/timeToast/timeToast/service/jwt/JwtServiceImpl.java
@@ -3,11 +3,13 @@ package com.timeToast.timeToast.service.jwt;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.timeToast.timeToast.domain.member.LoginMember;
+import com.timeToast.timeToast.domain.member_jwt_refresh.MemberJwtRefreshToken;
 import com.timeToast.timeToast.dto.member.LoginResponse;
 import com.timeToast.timeToast.global.exception.InternalServerException;
 import com.timeToast.timeToast.service.member_jwt_refresh.MemberJwtRefreshTokenService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -22,6 +24,7 @@ import static com.timeToast.timeToast.global.constant.TimeConstant.ONE_DAY;
 
 
 @Service
+@Slf4j
 public class JwtServiceImpl implements JwtService {
 
     private final MemberJwtRefreshTokenService memberJwtRefreshTokenService;
@@ -37,8 +40,9 @@ public class JwtServiceImpl implements JwtService {
     public LoginResponse createJwts(final LoginMember loginMember) {
         String accessToken = createToken(loginMember, ONE_HOUR.time());
         String refreshToken = createToken(loginMember, ONE_DAY.time());
-        System.out.println("login member: " + loginMember.id());
+
         memberJwtRefreshTokenService.save(loginMember.id(), refreshToken);
+        log.info("login by {}", loginMember.id());
 
         return LoginResponse.of(accessToken, refreshToken);
     }
