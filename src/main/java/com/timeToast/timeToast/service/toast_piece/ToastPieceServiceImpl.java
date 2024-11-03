d6bab27dca9468acc7edbdbda8e2dd95ba2be1b6
yeiin <yeini0417@gmail.com>
2024-11-04 00:32:41 +0900

    feat: toastpiece delete

--- src/main/java/com/timeToast/timeToast/service/toast_piece/ToastPieceServiceImpl.java
+++ src/main/java/com/timeToast/timeToast/service/toast_piece/ToastPieceServiceImpl.java
@@ -8,6 +8,7 @@ import com.timeToast.timeToast.dto.toast_piece.response.ToastPieceResponse;
 import com.timeToast.timeToast.dto.toast_piece.response.ToastPieceResponses;
 import com.timeToast.timeToast.dto.toast_piece.response.ToastPieceSaveResponse;
 import com.timeToast.timeToast.global.exception.BadRequestException;
+import com.timeToast.timeToast.global.exception.NotFoundException;
 import com.timeToast.timeToast.repository.gift_toast.gift_toast_owner.GiftToastOwnerRepository;
 import com.timeToast.timeToast.repository.toast_piece.toast_piece.ToastPieceRepository;
 import com.timeToast.timeToast.repository.toast_piece.toast_piece_image.ToastPieceImageRepository;
@@ -18,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.timeToast.timeToast.global.constant.ExceptionConstant.INVALID_TOAST_PIECE;
 import static com.timeToast.timeToast.global.constant.ExceptionConstant.TOAST_PIECE_NOT_EXISTS;
 
 @Service
@@ -40,6 +42,8 @@ public class ToastPieceServiceImpl implements ToastPieceService{
         ToastPiece toastPiece = toastPieceRepository.saveToastPiece(
           ToastPieceRequest.to(memberId, toastPieceRequest)
         );
+
+        System.out.println(toastPiece.toString());
 
         return ToastPieceSaveResponse.from(toastPiece);
     }
@@ -96,6 +100,20 @@ public class ToastPieceServiceImpl implements ToastPieceService{
 
         toastPieceImages.forEach(toastPieceImage -> toastPieceImageRepository.deleteToastPieceImage(toastPieceImage));
         toastPieceRepository.deleteToastPiece(toastPiece);
+    }
+
+    @Transactional
+    @Override
+    public void deleteToastPieceByMemberId(long memberId, long toastPieceId) {
+        ToastPiece toastPiece = toastPieceRepository.findById(toastPieceId).orElseThrow(()-> new NotFoundException(TOAST_PIECE_NOT_EXISTS.getMessage()));
+
+        if(toastPiece.getMemberId().equals(memberId)){
+            toastPieceRepository.deleteToastPiece(toastPiece);
+        }else {
+            throw new BadRequestException(INVALID_TOAST_PIECE.getMessage());
+        }
+
+
     }
 
 }
