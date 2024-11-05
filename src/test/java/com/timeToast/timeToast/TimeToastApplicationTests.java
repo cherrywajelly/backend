53b7e03e0c16766ff5723bb254d377ec37807a2b
yeiin <yeini0417@gmail.com>
2024-11-05 14:40:53 +0900

    feat: test containers

--- src/test/java/com/timeToast/timeToast/TimeToastApplicationTests.java
+++ src/test/java/com/timeToast/timeToast/TimeToastApplicationTests.java
@@ -1,12 +1,16 @@
 package com.timeToast.timeToast;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
 
 @SpringBootTest
 class TimeToastApplicationTests {
+
 	@Test
 	void contextLoads() {
 	}
