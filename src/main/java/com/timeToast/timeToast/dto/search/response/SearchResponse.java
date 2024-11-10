334bc97c84762924d0cb32b0ef308ed34586bb58
yeiin <yeini0417@gmail.com>
2024-11-10 23:16:45 +0900

    feat: default profile Image

--- src/main/java/com/timeToast/timeToast/dto/search/response/SearchResponse.java
+++ src/main/java/com/timeToast/timeToast/dto/search/response/SearchResponse.java
@@ -1,6 +1,7 @@
 package com.timeToast.timeToast.dto.search.response;
 
 import com.timeToast.timeToast.domain.member.member.Member;
+import com.timeToast.timeToast.service.icon.icon.IconService;
 import lombok.Builder;
 
 @Builder
@@ -11,11 +12,12 @@ public record SearchResponse(
 
 ) {
 
-    public static SearchResponse from(final Member member){
+    public static SearchResponse from(final Member member, final String profileUrl){
+
         return SearchResponse.builder()
                 .memberId(member.getId())
                 .nickname(member.getNickname())
-                .profileUrl(member.getMemberProfileUrl())
+                .profileUrl(profileUrl)
                 .build();
     }
 }
--- src/main/java/com/timeToast/timeToast/service/search/SearchServiceImpl.java
+++ src/main/java/com/timeToast/timeToast/service/search/SearchServiceImpl.java
@@ -3,28 +3,44 @@ package com.timeToast.timeToast.service.search;
 import com.timeToast.timeToast.dto.search.SearchRequest;
 import com.timeToast.timeToast.dto.search.response.SearchResponse;
 import com.timeToast.timeToast.dto.search.response.SearchResponses;
+import com.timeToast.timeToast.repository.icon.icon.IconRepository;
 import com.timeToast.timeToast.repository.member.member.MemberRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class SearchServiceImpl implements SearchService{
 
     private final MemberRepository memberRepository;
+    private final IconRepository iconRepository;
 
-    public SearchServiceImpl(final MemberRepository memberRepository) {
+    public SearchServiceImpl(final MemberRepository memberRepository, final IconRepository iconRepository) {
         this.memberRepository = memberRepository;
+        this.iconRepository = iconRepository;
     }
 
     @Transactional(readOnly = true)
     @Override
     public SearchResponses searchNickname(final SearchRequest searchRequest) {
         Pageable pageable = PageRequest.of(searchRequest.page(), searchRequest.size());
-        List<SearchResponse> searchMembers = memberRepository.findMemberByNickname(searchRequest.searchKeyword(), pageable).stream().map(SearchResponse::from).toList();
+
+        List<SearchResponse> searchMembers = new ArrayList<>();
+        memberRepository.findMemberByNickname(searchRequest.searchKeyword(), pageable).forEach(
+                member -> {
+                    String profileUrl = member.getMemberProfileUrl();
+                    if(profileUrl == null){
+                        profileUrl = iconRepository.getDefaultIcon().getIconImageUrl();
+                    }
+                    searchMembers.add(SearchResponse.from(member, profileUrl));
+                }
+        );
+
+
         return new SearchResponses(searchRequest.page()+1,searchRequest.size(), searchMembers);
     }
 }
