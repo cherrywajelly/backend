eb675c7e13eea9ebb1d901661178212210726f26
munyeongJung <https://github.com/munyeong0103>
2024-11-22 17:20:12 +0900

    feat: add file error handling code

--- src/main/java/com/timeToast/timeToast/service/image/FileUploadServiceImpl.java
+++ src/main/java/com/timeToast/timeToast/service/image/FileUploadServiceImpl.java
@@ -3,6 +3,7 @@ package com.timeToast.timeToast.service.image;
 import com.oracle.bmc.objectstorage.ObjectStorageClient;
 import com.oracle.bmc.objectstorage.requests.PutObjectRequest;
 import com.timeToast.timeToast.global.config.OsClientConfiguration;
+import com.timeToast.timeToast.global.exception.BadRequestException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,10 +11,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 @Service
 @Slf4j
@@ -48,11 +47,13 @@ public class FileUploadServiceImpl implements FileUploadService {
                             .build();
 
             ObjectStorageClient storage = ociConfig.getObjectStorage();
+
             if (storage != null) {
+                log.info("file still uploading");
                 ociConfig.getObjectStorage().putObject(putObjectRequest);
                 return urlPrefix + putObjectRequest.getObjectName();
             } else {
-                log.error("ObjectStorage_client_is_null. Cannot perform putObject.");
+                throw new BadRequestException("ObjectStorage_client_is_null. Cannot perform putObject.");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -63,7 +64,6 @@ public class FileUploadServiceImpl implements FileUploadService {
                 throw new RuntimeException(e);
             }
         }
-        return null;
     }
 
 }
