f3a101cd051a035d00e59cd5bf961af9ca15c1bc
yeiin <yeini0417@gmail.com>
2024-11-25 01:44:35 +0900

    feat: member role

---
+++ src/test/java/com/timeToast/timeToast/util/WithMockCustomManager.java
@@ -0,0 +1,17 @@
+package com.timeToast.timeToast.util;
+
+import com.timeToast.timeToast.domain.enums.member.MemberRole;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockCustomManager {
+    long userId() default 1;
+    String userEmail() default "test email";
+    String userNickname() default "test nickname";
+    MemberRole userRole() default MemberRole.MANAGER;
+
+}
