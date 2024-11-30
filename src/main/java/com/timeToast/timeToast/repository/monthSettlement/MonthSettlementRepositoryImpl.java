ac9584275e3efbb456a8dcd6dddf2b98484a83d1
yeiin <yeini0417@gmail.com>
2024-11-30 15:03:34 +0900

    feat: month settle

--- src/main/java/com/timeToast/timeToast/repository/monthSettlement/MonthSettlementRepositoryImpl.java
+++ src/main/java/com/timeToast/timeToast/repository/monthSettlement/MonthSettlementRepositoryImpl.java
@@ -1,6 +1,10 @@
 package com.timeToast.timeToast.repository.monthSettlement;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MonthSettlementRepositoryImpl implements MonthSettlementRepository{
 
+    private MonthSettlementJpaRepository monthSettlementJpaRepository;
 
 }
