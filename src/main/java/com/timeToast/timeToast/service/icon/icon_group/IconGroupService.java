2540f6202036bb789e67084ffdec93a8c150ad20
yeiin <yeini0417@gmail.com>
2024-12-01 20:06:35 +0900

    fix: isBuy logic 수정

--- src/main/java/com/timeToast/timeToast/service/icon/icon_group/IconGroupService.java
+++ src/main/java/com/timeToast/timeToast/service/icon/icon_group/IconGroupService.java
@@ -11,6 +11,6 @@ public interface IconGroupService {
     IconGroupResponses getJamIconGroups(final long memberId);
     IconGroupMarketResponses getAllToastsIconGroups(final long memberId);
     IconGroupMarketResponses getAllJamsIconGroups(final long memberId);
-    IconGroupMarketDetailResponse getIconGroupDetail(final long iconGroupId);
+    IconGroupMarketDetailResponse getIconGroupDetail(final long memberId,final long iconGroupId);
     Response deleteIconGroup(final long memberId, final long iconGroupId);
 }
