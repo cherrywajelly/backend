51170c1fdd531747910b0694253c4e5aad1fbf30
yeiin <yeini0417@gmail.com>
2024-12-02 16:35:36 +0900

    test: memberToken unit Test

---
+++ src/test/java/com/timeToast/timeToast/service/withdrawal/WithdrawalServiceTest.java
@@ -0,0 +1,23 @@
+package com.timeToast.timeToast.service.withdrawal;
+
+import com.timeToast.timeToast.global.constant.StatusCode;
+import com.timeToast.timeToast.global.constant.SuccessConstant;
+import com.timeToast.timeToast.global.response.Response;
+
+public class WithdrawalServiceTest implements WithdrawalService{
+
+    @Override
+    public Response memberWithdrawal(long memberId) {
+        return new Response(StatusCode.OK.getStatusCode(), SuccessConstant.SUCCESS_DELETE.getMessage());
+    }
+
+    @Override
+    public Response creatorWithdrawal(long memberId) {
+        return new Response(StatusCode.OK.getStatusCode(), SuccessConstant.SUCCESS_DELETE.getMessage());
+    }
+
+    @Override
+    public Response adminWithdrawal(long memberId) {
+        return new Response(StatusCode.OK.getStatusCode(), SuccessConstant.SUCCESS_DELETE.getMessage());
+    }
+}
