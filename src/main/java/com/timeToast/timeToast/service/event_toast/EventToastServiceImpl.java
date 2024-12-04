24ac5a15709535783e1018e825f65b8d635cdda2
yeiin <yeini0417@gmail.com>
2024-12-04 23:44:06 +0900

    feat: giftToast 추가

--- src/main/java/com/timeToast/timeToast/service/event_toast/EventToastServiceImpl.java
+++ src/main/java/com/timeToast/timeToast/service/event_toast/EventToastServiceImpl.java
@@ -59,7 +59,7 @@ public class EventToastServiceImpl implements EventToastService{
     @Override
     public ResponseWithId saveEventToast(final EventToastPostRequest eventToastPostRequest, final long memberId) {
 
-        if(!StringValidator.stringValidation(eventToastPostRequest.title())){
+        if(!StringValidator.stringValidation(eventToastPostRequest.title()) || eventToastPostRequest.title().length() > 20) {
             throw new BadRequestException(INVALID_EVENT_TOAST.getMessage());
         }
 
