5fa5e3b3ea8f6e36517c19cfcf66a0681e195c59
munyeong <skymy02@ajou.ac.kr>
2024-12-07 14:36:56 +0900

    feat: add get eventToast dto (#256)

--- src/main/java/com/timeToast/timeToast/dto/event_toast/response/EventToastManagerResponse.java
+++ src/main/java/com/timeToast/timeToast/dto/event_toast/response/EventToastManagerResponse.java
@@ -1,20 +1,31 @@
 package com.timeToast.timeToast.dto.event_toast.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.timeToast.timeToast.domain.event_toast.EventToast;
 import lombok.Builder;
+
+import java.time.LocalDate;
 
 @Builder
 public record EventToastManagerResponse (
         long eventToastId,
         String iconImageUrl,
         String title,
-        String nickname
+        String nickname,
+        LocalDate openedDate,
+        boolean isOpened,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+        LocalDate createdAt
 ) {
-    public static EventToastManagerResponse from(final long eventToastId, final String title, final String iconImageUrl, final String nickname) {
+    public static EventToastManagerResponse from(final EventToast eventToast, final String iconImageUrl, final String nickname) {
         return EventToastManagerResponse.builder()
-                .eventToastId(eventToastId)
-                .title(title)
+                .eventToastId(eventToast.getId())
+                .title(eventToast.getTitle())
                 .iconImageUrl(iconImageUrl)
                 .nickname(nickname)
+                .openedDate(eventToast.getOpenedDate())
+                .isOpened(eventToast.isOpened())
+                .createdAt(eventToast.getCreatedAt().toLocalDate())
                 .build();
     }
 }
